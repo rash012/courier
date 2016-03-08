cafe8cb95598124a01afc5355f32a55ebdadbcab
Rash012 <rash012@mail.ru>
2016-03-08 17:23:38 +0300

    commit

--- client/templates/orders/orders_list.js
+++ client/templates/orders/orders_list.js
@@ -1,17 +1,5 @@
-var ordersData = [
-  {
-    to: 'Московская, 18',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    to: 'Московская, 100',
-    url: 'http://meteor.com'
-  },
-  {
-    to: 'Володарского, 5',
-    url: 'http://themeteorbook.com'
-  }
-];
 Template.ordersList.helpers({
-  posts: ordersData
+  orders: function () {
+    return Orders.find();
+  }
 });
